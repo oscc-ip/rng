91e9d24f038997f97608ab295fa685cbae1eca69
maksyuki <maksyuki@126.com>
2023-11-21 15:57:49 +0800

    chore: fix some signal typos

--- rtl/apb4_rng.sv
+++ rtl/apb4_rng.sv
@@ -39,27 +39,27 @@ module apb4_rng #(
     // verilog_format: on
 );
 
-  logic [3:0] s_apb_addr;
+  logic [3:0] s_apb4_addr;
   logic s_apb4_wr_hdshk, s_apb4_rd_hdshk;
   logic [DATA_WIDTH-1:0] s_rng_ctrl_d, s_rng_ctrl_q;
   logic [DATA_WIDTH-1:0] s_rng_val;
 
-  assign s_apb_addr = apb4.paddr[5:2];
+  assign s_apb4_addr = apb4.paddr[5:2];
   assign s_apb4_wr_hdshk = apb4.psel && apb4.penable && apb4.pwrite;
   assign s_apb4_rd_hdshk = apb4.psel && apb4.penable && (~apb4.pwrite);
 
-  assign s_rng_ctrl_d = (s_apb4_wr_hdshk && s_apb_addr == `RNG_CTRL) ? apb4.pwdata : s_rng_ctrl_q;
+  assign s_rng_ctrl_d = (s_apb4_wr_hdshk && s_apb4_addr == `RNG_CTRL) ? apb4.pwdata : s_rng_ctrl_q;
   dffr u_rng_ctrl_dffr (
-      apb4.hclk,
-      apb4.hresetn,
+      apb4.pclk,
+      apb4.presetn,
       s_rng_ctrl_d,
       s_rng_ctrl_q
   );
 
   lfsr_galois #(32, 32'h04C1_1DB7) u_lfsr_galois (
-      .clk_i  (apb4.hclk),
-      .rst_n_i(apb4.hresetn),
-      .wr_i   (s_apb4_wr_hdshk && s_apb_addr == `RNG_SEED),
+      .clk_i  (apb4.pclk),
+      .rst_n_i(apb4.presetn),
+      .wr_i   (s_apb4_wr_hdshk && s_apb4_addr == `RNG_SEED),
       .dat_i  (s_rng_seed),
       .dat_o  (s_rng_val)
   );
@@ -67,7 +67,7 @@ module apb4_rng #(
   always_comb begin
     apb4.prdata = '0;
     if (s_apb4_rd_hdshk) begin
-      unique case (s_apb_addr)
+      unique case (s_apb4_addr)
         `RNG_CTRL: apb4.prdata[DATA_WIDTH-1:0] = s_rng_ctrl_q;
         `RNG_VAL:  apb4.prdata[DATA_WIDTH-1:0] = s_rng_seed;
       endcase
@@ -75,6 +75,6 @@ module apb4_rng #(
   end
 
   assign apb4.pready = 1'b1;
-  assign apb4.pslerr = 1'b0;
+  assign apb4.pslverr = 1'b0;
 
 endmodule
