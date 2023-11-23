489fdc1c9db44775929bc1554ba4f8447f6aec64
maksyuki <maksyuki@126.com>
2023-11-23 09:47:17 +0800

    feat: modify rng modules

--- rtl/apb4_rng.sv
+++ rtl/apb4_rng.sv
@@ -1,5 +1,5 @@
 // Copyright (c) 2023 Beijing Institute of Open Source Chip
-// timer is licensed under Mulan PSL v2.
+// rng is licensed under Mulan PSL v2.
 // You can use this software according to the terms and conditions of the Mulan PSL v2.
 // You may obtain a copy of Mulan PSL v2 at:
 //             http://license.coscl.org.cn/MulanPSL2
@@ -8,59 +8,39 @@
 // MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 // See the Mulan PSL v2 for more details.
 
-// verilog_format: off
-`define RNG_CTRL 4'b0000 //BASEADDR+0x00
-`define RNG_SEED 4'b0001 //BASEADDR+0x04
-`define RNG_VAL  4'b0010 //BASEADDR+0x08
-// verilog_format: on
+`include "register.sv"
+`include "lfsr.sv"
+`include "rng_define.sv"
 
-/* register mapping
- * RNG_CTRL:
- * BITS:   | 31:0 |
- * FIELDS: | RES  |
- * PERMS:  | RW   |
- * ------------------------------------
- * RNG_SEED:
- * BITS:   | 31:0 |
- * FIELDS: | SEED |
- * PERMS:  | W    |
- * ------------------------------------
- * RNG_VAL:
- * BITS:   | 31:0 |
- * FIELDS: | VAL  |
- * PERMS:  | R    |
-*/
-
-module apb4_rng #(
-    parameter int DATA_WIDTH = 32
-) (
-    // verilog_format: off
+module apb4_rng (
     apb4_if.slave apb4
-    // verilog_format: on
 );
 
   logic [3:0] s_apb4_addr;
   logic s_apb4_wr_hdshk, s_apb4_rd_hdshk;
-  logic [DATA_WIDTH-1:0] s_rng_ctrl_d, s_rng_ctrl_q;
-  logic [DATA_WIDTH-1:0] s_rng_val;
+  logic [`RNG_CTRL_WIDTH-1:0] s_rng_ctrl_d, s_rng_ctrl_q;
+  logic [`RNG_VAL_WIDTH-1:0] s_rng_val;
 
   assign s_apb4_addr = apb4.paddr[5:2];
   assign s_apb4_wr_hdshk = apb4.psel && apb4.penable && apb4.pwrite;
   assign s_apb4_rd_hdshk = apb4.psel && apb4.penable && (~apb4.pwrite);
+  assign apb4.pready = 1'b1;
+  assign apb4.pslverr = 1'b0;
 
-  assign s_rng_ctrl_d = (s_apb4_wr_hdshk && s_apb4_addr == `RNG_CTRL) ? apb4.pwdata : s_rng_ctrl_q;
-  dffr u_rng_ctrl_dffr (
+  assign s_rng_ctrl_d = (s_apb4_wr_hdshk && s_apb4_addr == `RNG_CTRL) ? apb4.pwdata[`RNG_CTRL_WIDTH-1:0] : s_rng_ctrl_q;
+  dffr #(`RNG_CTRL_WIDTH) u_rng_ctrl_dffr (
       apb4.pclk,
       apb4.presetn,
       s_rng_ctrl_d,
       s_rng_ctrl_q
   );
 
-  lfsr_galois #(32, 32'h04C1_1DB7) u_lfsr_galois (
+  // 32bits m-sequence
+  lfsr_galois #(`RNG_VAL_WIDTH, 32'hE000_0200) u_lfsr_galois (
       .clk_i  (apb4.pclk),
       .rst_n_i(apb4.presetn),
-      .wr_i   (s_apb4_wr_hdshk && s_apb4_addr == `RNG_SEED),
-      .dat_i  (s_rng_seed),
+      .wr_i   (s_apb4_wr_hdshk && s_apb4_addr == `RNG_SEED && s_rng_ctrl_q[0]),
+      .dat_i  (apb4.pwdata),
       .dat_o  (s_rng_val)
   );
 
@@ -68,13 +48,11 @@ module apb4_rng #(
     apb4.prdata = '0;
     if (s_apb4_rd_hdshk) begin
       unique case (s_apb4_addr)
-        `RNG_CTRL: apb4.prdata[DATA_WIDTH-1:0] = s_rng_ctrl_q;
-        `RNG_VAL:  apb4.prdata[DATA_WIDTH-1:0] = s_rng_seed;
+        `RNG_CTRL: apb4.prdata[`RNG_CTRL_WIDTH-1:0] = s_rng_ctrl_q;
+        `RNG_VAL:  apb4.prdata[`RNG_VAL_WIDTH-1:0] = s_rng_val;
+        default:   apb4.prdata = '0;
       endcase
     end
   end
-
-  assign apb4.pready = 1'b1;
-  assign apb4.pslverr = 1'b0;
 
 endmodule
---
+++ rtl/rng_define.sv
@@ -0,0 +1,46 @@
+// Copyright (c) 2023 Beijing Institute of Open Source Chip
+// rng is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+`ifndef INC_RNG_DEF_SV
+`define INC_RNG_DEF_SV
+
+/* register mapping
+ * RNG_CTRL:
+ * BITS:   | 31:1 | 0  |
+ * FIELDS: | RES  | EN |
+ * PERMS:  | RW   | RW |
+ * ---------------------
+ * RNG_SEED:
+ * BITS:   | 31:0 |
+ * FIELDS: | SEED |
+ * PERMS:  | W    |
+ * ---------------------
+ * RNG_VAL:
+ * BITS:   | 31:0 |
+ * FIELDS: | VAL  |
+ * PERMS:  | R    |
+ * ---------------------
+*/
+
+// verilog_format: off
+`define RNG_CTRL 4'b0000 // BASEADDR + 0x00
+`define RNG_SEED 4'b0001 // BASEADDR + 0x04
+`define RNG_VAL  4'b0010 // BASEADDR + 0x08
+
+`define RNG_CTRL_ADDR {26'b0, `RNG_CTRL, 2'b00}
+`define RNG_SEED_ADDR {26'b0, `RNG_SEED, 2'b00}
+`define RNG_VAL_ADDR  {26'b0, `RNG_VAL , 2'b00}
+
+`define RNG_CTRL_WIDTH 1
+`define RNG_SEED_WIDTH 32
+`define RNG_VAL_WIDTH  32
+// verilog_format: on
+
+`endif
